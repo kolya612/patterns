c0bfbc046e53c942b6137f36caa1f093f46a05d1
Kolya612 <kolya612@gmail.com>
2021-05-03 20:48:59 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+require 'libs/ko612/loger.php';
+use Libs\Ko612\Logger as Logger;
+
+Logger::log("И снова привет!!!");
---
+++ libs/ko612/loger.php
@@ -0,0 +1,43 @@
+<?php
+namespace libs\ko612;
+
+require_once 'libs\ko612\singleton.php';
+
+class Logger extends Singleton
+{
+    /**
+     * Ресурс указателя файла файла журнала.
+     */
+    private $fileHandle;
+
+    /**
+     * Поскольку конструктор Одиночки вызывается только один раз, постоянно
+     * открыт всего лишь один файловый ресурс.
+     *
+     * Обратите внимание, что для простоты мы открываем здесь консольный поток
+     * вместо фактического файла.
+     */
+    protected function __construct()
+    {
+        $this->fileHandle = fopen('log.txt', 'a');
+    }
+
+    /**
+     * Пишем запись в журнале в открытый файловый ресурс.
+     */
+    public function writeLog(string $message): void
+    {
+        $date = date('Y-m-d H.m.s');
+        fwrite($this->fileHandle, "$date: $message\n");
+    }
+
+    /**
+     * Просто удобный ярлык для уменьшения объёма кода, необходимого для
+     * регистрации сообщений из клиентского кода.
+     */
+    public static function log(string $message): void
+    {
+        $logger = static::getInstance();
+        $logger->writeLog($message);
+    }
+}
